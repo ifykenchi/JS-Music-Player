cbf7f39f2b554ba339b26946328c20b8a917cb69
ifykenchi <ifykenchi@gmail.com>
2023-10-23 16:01:21 +0100

    updated the progress bar

--- script.js
+++ script.js
@@ -28,12 +28,54 @@ function playSong() {
     musicContainer.classList.add('play')
     playBtn.querySelector('i.fas').classList.remove('fa-play')
     playBtn.querySelector('i.fas').classList.add('fa-pause')
+
+    audio.play();
 }
 
 function pauseSong() {
     musicContainer.classList.remove('play')
     playBtn.querySelector('i.fas').classList.remove('fa-pause')
     playBtn.querySelector('i.fas').classList.add('fa-play')
+
+    audio.pause();
+}
+
+function prevSong() {
+    songIndex--;
+
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+    
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+function nextSong() {
+    songIndex++;
+
+    if (songIndex >= songs.length) {
+        songIndex = 0;
+    }
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+function updateProgress(e) {
+    const {duration, currentTime} = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+}
+
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX / width) * duration;
 }
 
 //Event listeners
@@ -47,4 +89,12 @@ playBtn.addEventListener('click', () => {
     }
 });
 
+//Change song events
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
 
+audio.addEventListener('timeupdate', updateProgress)
+
+progressContainer.addEventListener('click', setProgress);
+
+audio.addEventListener('ended', nextSong);
